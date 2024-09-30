61f72958331e8bcba15a1081330e65dae0c756cc
TristanJones08 <tjones0@ftstudent.org>
2024-09-30 14:08:18 -0400

    New Stuff
    New Stuff 9/30

--- Towers/RedTowerPanel.gd
+++ Towers/RedTowerPanel.gd
@@ -12,15 +12,27 @@ func _on_gui_input(event):
 
 		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
 
+		tempTower.scale = Vector2(0.32,0.32)
+
 	elif event is InputEventMouseMotion and event.button_mask == 1:
-		get_child(1).global_position = event.global_position
+		if get_child_count()>1:
+			get_child(1).global_position = event.global_position
+
+			var mapPath = get_tree().get_root().get_node("Main/TileMap")
 
 	elif event is InputEventMouseButton and event.button_mask == 0:
-		print("Left button Up")
-		get_child(1).queue_free()
+		if event.global_position.x >= 2944:
+			if get_child_count()>1:
+				get_child(1).queue_free()
+			else:
+				if get_child_count() > 1:
+					get_child(1).queue_free()
 		var path = get_tree().get_root().get_node("Main/Towers")
 
 		path.add_child(tempTower)
 		tempTower.global_position = event.global_position
-		tempTower.get_node("Area").hide()
+		
+	else:
+		if get_child_count()> 1:
+			get_child(1).queue_free()
 
