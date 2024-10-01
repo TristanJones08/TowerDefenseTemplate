c27333cb8933b7e3caad3bc917265d905f87daec
TristanJones08 <tjones0@ftstudent.org>
2024-10-01 14:05:19 -0400

    NwT
    New things 10/1

--- Towers/RedTowerPanel.gd
+++ Towers/RedTowerPanel.gd
@@ -16,21 +16,29 @@ func _on_gui_input(event):
 
 	elif event is InputEventMouseMotion and event.button_mask == 1:
 		if get_child_count()>1:
-			get_child(1).global_position = event.global_position
+			get_child(1).global_position = get_global_mouse_position()
 
 			var mapPath = get_tree().get_root().get_node("Main/TileMap")
+			var tile = mapPath.local_to_map(get_global_mouse_position())
+			currTile = mapPath.get_cell_atlas_coords(0, tile, false)
+			if (currTile == Vector2i(4,5)):
+				get_child(1).get_node("Panel").modulate = Color.GREEN
+			else:
+				get_child(1).get_node("Panel").modulate = Color.RED
 
 	elif event is InputEventMouseButton and event.button_mask == 0:
-		if event.global_position.x >= 2944:
+		if get_global_mouse_position().x >= 2944:
 			if get_child_count()>1:
 				get_child(1).queue_free()
 			else:
 				if get_child_count() > 1:
 					get_child(1).queue_free()
+
 		var path = get_tree().get_root().get_node("Main/Towers")
 
 		path.add_child(tempTower)
-		tempTower.global_position = event.global_position
+		tempTower.global_position = get_global_mouse_position()
+		tempTower.get_node("Panel").hide()
 		
 	else:
 		if get_child_count()> 1:
